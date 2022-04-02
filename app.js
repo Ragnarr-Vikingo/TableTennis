432f3ab039940b718ae150cad146a168fffc2297
Ragnarr Vikingo <martin.thompson@hakshark.com>
2022-04-02 17:33:34 +0100

    working on logic of serve or return

--- app.js
+++ app.js
@@ -1,40 +1,91 @@
+const p1Games = document.querySelector( '#p1-games' );
+const p1Points = document.querySelector( '#p1-points' );
 
-const tossOutcome = document.querySelector( "#tossOutcome" );
+const p2Games = document.querySelector( '#p2-games' );
+const p2Points = document.querySelector( '#p2-points' );
 
-const btnPlayer1 = document.querySelector( "#btnPlayer1" );
-const btnPlayer2 = document.querySelector( "#btnPlayer2" );
+const servicePlayer = document.querySelector( '#service-player');
 
-const btnPlayer1Bat = document.querySelector( "#btnPlayer1Bat" );
-const btnPlayer2Bat = document.querySelector( "#btnPlayer2Bat" );
+const btnPlayerOne = document.querySelector( '#btn-playerOne' );
+const btnPlayerTwo = document.querySelector( '#btn-playerTwo' );
 
-btnPlayer1.addEventListener( 'click', (evt) => {
-    console.log('Player One chooses HEADs...');
-    let randNum = Math.random();
-    if ( randNum < 0.5 ) {
-        tossOutcome.innerHTML= 'Coin landed HEADs side up!, Player One gets to serve First!!';
-        btnPlayer1Bat.classList.add( 'is-info' );
-        btnPlayer2Bat.disabled = true;
+let p1PointsScore = 0;
+let p2PointsScore = 0;
+
+let serviceCount = 1;
+let rallyCount = 0;
+
+let serviceIsWith = 0;
+let returnsBall = 0;
+
+window.onload = (event) => {
+    // Lets set up for a new game
+    p1Games.outerHTML = "0";
+    p1Points.outerHTML = "0";
+    p2Games.outerHTML = "0";
+    p2Points.outerHTML = "0";
+
+    serviceIsWith = Math.floor(Math.random() * 2) + 1;
+    if ( serviceIsWith === 1 ) {
+        servicePlayer.outerHTML = 'One';
+        btnPlayerTwo.disabled = true;
+        btnPlayerOne.classList.add( 'is-success' );
     } else {
-        tossOutcome.innerHTML = `Coin landed TAILs side up!, Player Two gets to serve First!!`;  
-        btnPlayer2Bat.classList.add( 'is-info' );
-        btnPlayer1Bat.disabled = true;    
+        servicePlayer.outerHTML = 'Two';
+        btnPlayerOne.disabled = true;
+        btnPlayerTwo.classList.add( 'is-success' );
+    }
+};
+
+btnPlayerOne.addEventListener( 'click', (evt) => {
+    // Are we serving or returning?
+    if ( serviceIsWith === 1 ) {
+        if ( rallyCount === 0 ) {
+            if ( serviceCount === 1 ) {
+                console.log('Player One: First Service...');
+                serviceCount++;
+            } else {
+                console.log('Player One: Second Service...');
+            }
+            btnPlayerOne.disabled = true;
+            btnPlayerTwo.disabled = false;    
+            btnPlayerOne.classList.remove( 'is-success' );
+            btnPlayerTwo.classList.add( 'is-success' );
+        } else {
+            returnsBall = Math.floor(Math.random() * 2) + 1;
+            if ( returnsBall === 1 ) {
+                console.log('Player One: Ball missed...');
+                console.log('Point to Player Two...');
+                rallyCount = 0;
+                p2PointsScore++;
+
+            } else {
+                console.log('Player One: Returns ball...[I am currently server]');
+                rallyCount++;
+                btnPlayerOne.disabled = true;
+                btnPlayerTwo.disabled = false;    
+                btnPlayerOne.classList.remove( 'is-success' );
+                btnPlayerTwo.classList.add( 'is-success' );
+            }
+        }
+    } else {
+        console.log('Player One: Returns ball...[Im not server]');
+        rallyCount++; 
+        btnPlayerOne.disabled = true;
+        btnPlayerTwo.disabled = false;    
+        btnPlayerOne.classList.remove( 'is-success' );
+        btnPlayerTwo.classList.add( 'is-success' );       
     }
 })
+   
 
-btnPlayer2.addEventListener( 'click', (evt) => {
-    console.log('Player Two chooses HEADs...'); 
-    let randNum = Math.random();
-    if ( randNum < 0.5 ) {
-        tossOutcome.innerHTML= 'Coin landed HEADs side up!, Player Two gets to serve First!!';
-        btnPlayer2Bat.classList.add( 'is-info' );
-        btnPlayer1Bat.disabled = true;
-    } else {
-        tossOutcome.innerHTML = `Coin landed TAILs side up!, Player One gets to serve First!!`;  
-        btnPlayer1Bat.classList.add( 'is-info' );
-        btnPlayer2Bat.disabled = true;    
-    }  
+
+
+btnPlayerTwo.addEventListener( 'click', (evt) => {
+    rallyCount++;
+    console.log('Player Two: Returns ball...')
+    btnPlayerTwo.disabled = true;
+    btnPlayerOne.disabled = false;
+    btnPlayerTwo.classList.remove( 'is-success' );
+    btnPlayerOne.classList.add( 'is-success' );
 })
-
-
-
-
